e0bfbfe1c78ce681b88d7adc0967b01d518e6ffd
GregRgy <gregrgy12@outlook.com>
2023-06-04 11:54:19 +0200

    écriture du cart.js 40% et corrections apportées au product.js lors de l'ajout d'un canapé notamment

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,9 +1,132 @@
-// variable qui contiendra les keys et les values du localStorage
-let productRegisterLocalStorage = JSON.parse(localStorage.getItem("product"));
-    console.log(productRegisterLocalStorage)
-// balise dans laquelle on va insérer les produits et leurs informations
-const productsPositionHtml = document.getElementById("cart__items");
+const basketValue = JSON.parse(localStorage.getItem("product"));
 
-async function fecthApi(){
-    
+async function fetchApi() {
+  let basketArrayFull = [];
+  let basketClassFull = JSON.parse(localStorage.getItem("product"));
+  if (basketClassFull !== null) {
+    for (let i = 0; i < basketClassFull.length; i++) {
+      await fetch(
+        "http://localhost:3000/api/products/" +
+          basketClassFull[i].idSelectedProduct
+      )
+        .then((res) => res.json())
+        .then((kanap) => {
+          const article = {
+            _id: kanap._id,
+            name: kanap.name,
+            price: kanap.price,
+            color: basketClassFull[i].colorSelectedProduct,
+            quantity: basketClassFull[i].quantity,
+            alt: kanap.altTxt,
+            img: kanap.imageUrl,
+          };
+          basketArrayFull.push(article);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+  }
+  return basketArrayFull;
+}
+
+async function showBasket() {
+  const responseFetch = await fetchApi();
+  const basketValue = JSON.parse(localStorage.getitem("product"));
+  if (basketValue !== null && basketValue.length !== 0) {
+    const cartPage = document.querySelector("#cart__items");
+    responseFetch.forEach((product) => {
+      cartPage.innerHTML += `<article class="cart__item" data-id="${product._id}" data-color="${product.color}">
+            <div class="cart__item__img">
+              <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+            </div>
+            <div class="cart__item__content">
+              <div class="cart__item__content__description">
+                <h2>${product.name}</h2>
+                <p>${product.color}</p>
+                <p>${product.price} €</p>
+              </div>
+              <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                  <p>Qté : </p>
+                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                  <p class="deleteItem">Supprimer</p>
+                </div>
+              </div>
+            </div>
+          </article>`;
+    });
+  } else {
+    return messagePaniervide();
+  }
+}
+
+function getBasket() {
+  return JSON.parse(localStorage.getItem("product"));
+}
+
+async function modifyQuantity() {
+  await fetchApi;
+  const quantityinCart = document.querySelectorAll(".itemQuantity");
+  for (let input of quantityInCart) {
+    input.addEventListener("change", function () {
+      let basketvalue = getBasket();
+      let idModif = this.closest(".cart__item").dataset.id;
+      let colorModif = this.closest(".cart__item").dataset.color;
+      let findId = basketValue.filter(
+        (e) => e.colorSelectedProduct === idModif
+      );
+      let findColor = findId.find((e) => e.colorSelectedProduct === colorModif);
+      if (this.value > 0) {
+        findColor.quantity = this.value;
+        localStorage.setitem("product", JSON.stringify(basketValue));
+        calculTotalQuantity();
+        calculTotalPrice();
+      } else {
+        calculTotalQuantity();
+        calculTotalPrice();
+      }
+      localStorage.setitem("product", JSON.stringify(basketvalue));
+    });
+  }
+}
+
+async function removeItem() {
+  await fetchApi();
+  const kanapDelete = document.querySelectorAll(".deleteItem");
+  kanapDelete.forEach((article) => {
+    article.addEventListener("click", function (event) {
+      let basketvalue = getbasket();
+      const idDelete = e.target.closest("article").getAttribute("data-id");
+      const colorDelete = event.target
+        .closest("article")
+        .getAttribute("data-color");
+      const searchDeleteKanap = basketValue.find(
+        (element) =>
+          element.idSelectedProduct == idDelete &&
+          element.colorSelectedProduct == colorDelete
+      );
+      basketValue = basketValue.filter((item) => item != searchDeleteKanap);
+      localStorage.setitem("product", JSON.stringify(basketValue));
+      const getSection = document.querySelector("#cart__items");
+      getSection.removeChild(event.target.closest("article"));
+      alert("article supprimé !");
+      calculTotalQuantity();
+      calculTotalPrice();
+    });
+  });
+}
+removeItem();
+
+initialize();
+
+async function initialize(){
+  showBasket();
+  removeItem();
+  modifyQuantity();
+
+  calculTotalQuantity();
+  calculTotalPrice();
 }
--- front/js/product.js
+++ front/js/product.js
@@ -1,96 +1,90 @@
 let url = new URL(location.href); //création d'une varibale url étant l'url de la page actuelle
 let kanapPageId = url.searchParams.get("id"); //récupère l'id contenu dans l'url de la page actuelle
 
-const kanapImg = document.querySelector(".item__img");
-const kanapName = document.querySelector("#title");
-const kanapPrice = document.querySelector("#price");
-const kanapDescription = document.querySelector("#description");
-const colorOptions = document.querySelector("#colors");
-const getProductQuantity = document.querySelector("#quantity");
-
 fetch(`http://localhost:3000/api/products/${kanapPageId}`)
+  .then((res) => res.json())
+  .then((object) => {
+    for (let couleur of object.colors) {
+      document.querySelector(
+        "#colors"
+      ).innerHTML += `<option value="${couleur}">${couleur}</option>`;
+    }
+    document.querySelector(
+      ".item__img"
+    ).innerHTML += `<img src="${object.imageUrl}" alt="${object.altTxt}">`;
+    document.querySelector("#title").innerHTML += `${object.name}`;
+    document.querySelector("#price").innerHTML += `${object.price}`;
+    document.querySelector("#description").innerHTML += `${object.description}`;
+  })
+
+  .catch(function (err) {
+    console.log(err);
+  });
+
+const button = document.getElementById("addToCart");
+
+button.addEventListener("click", () => {
+  if (document.querySelector("#colors").value === "") {
+    alert("Veuillez choisir une couleur, SVP");
+  }
+  // Si la quantité choisie est nulle ou si elle dépasse 100
+  else if (
+    document.querySelector("#quantity").value <= 0 ||
+    document.querySelector("#quantity").value > 100
+  ) {
+    alert("Veuillez choisir une quantité corecte");
+  } else {
+    // Si tout est bon alors on envoie le panier au LocalStorage
+    addBasket();
+  }
+});
+
+function addBasket() {
+  let basketValue = getBasket();
+  let foundProducts = basketValue.find(
+    //foundProducts représente l'article à trouver
+    (item) =>
+      item.idSelectedProduct === kanapPageId &&
+      item.colorSelectedProduct === document.querySelector("#colors").value
+  ); //Ici il faut que les produits du panier et les produits du LS aient le même id et la même couleur sinon foundProducts sera égal à undefined
+
+  if (foundProducts == undefined) {
+    let product = {
+      // nous initions la nouvelle variable basketValue
+      idSelectedProduct: kanapPageId,
+      colorSelectedProduct: document.querySelector("#colors").value,
+      quantity: document.querySelector("#quantity").value,
+    };
+    basketValue.push(product);
+  } else {
+    let newQuantity =
+      parseInt(foundProducts.quantity) +
+      parseInt(document.querySelector("#quantity").value);
+    foundProducts.quantity = newQuantity;
+  }
+  fetch(`http://localhost:3000/api/products/${kanapPageId}`)
     .then((res) => res.json())
     .then((object) => {
-        for (let couleur of object.colors){
-            colorOptions.innerHTML += `<option value="${couleur}">${couleur}</option>`;
-        }
-        kanapImg.innerHTML += `<img src="${object.imageUrl}" alt="${object.altTxt}">`;
-        kanapName.innerHTML += `${object.name}`;
-        kanapPrice.innerHTML += `${object.price}`;
-        kanapDescription.innerHTML += `${object.description}`;
-        // Création du bouton ajouter au panier
-
-    const button = document.getElementById("addToCart");
-
-    // Local Storage
-
-    button.addEventListener("click", () => {
-        let basketValue = {
-            // nous initions la nouvelle variable basketValue
-            idSelectedProduct: kanapPageId,
-            nameSelectedProduct: kanapName,
-            colorSelectedProduct: colorOptions.value,
-            quantity: getProductQuantity.value,
-        };
-
-        // Il faut maintenant récupérer les données du panier
-
-        function getBasket(){
-            let basketValue = JSON.parse(localStorage.getItem("product"));
-            if (basketValue === null){
-                // return[] tableau vide si localstorage vide
-            } else {
-                return basketValue 
-            }
-        }
-
-        // Création de l'ajout au panier avec l'argument product
-
-        function addBasket(product){
-            let basketvalue = getBasket();
-            let foundProducts = basketValue.find( //foundProducts représente l'article à trouver
-                (item) =>
-                    item.idSelectedProduct === product.idSelectedProduct &&
-                    item.colorSelectedProduct === product.colorSelectedProduct
-            ); //Ici il faut que les produits du panier et les produits du LS aient le même id et la même couleur
-
-            if (
-                foundProducts == undefined &&
-                colorOptions.value != "" &&
-                getProductQuantity.value > 0 &&
-                getProductQuantity.value <= 100
-            ) {
-                product.quantity = getProductQuantity.value;
-                basketValue.push(product);
-            } else {
-                let newQuantity = 
-                    parseInt(foundProducts.quantity) +
-                    parseInt(getProductQuantity.value);
-                foundProducts.quantity = newQuantity;
-            }
-            saveBasket(basketValue);
-            alert(
-                `Le canapé ${nameKanap} ${colorOptions.value} a été ajouté en ${getProductQuantity.value} exemplaires à votre panier !`
-            );
-        }
-        function saveBasket(basketValue){
-            localStorage.setItem("product", JSON.stringify(basketValue));
-        }
-
-        if (colorOptions.value === ""){
-            alert("Veuillez choisir une couleur, SVP");
-        } 
-
-        else if (
-            getProductQuantity.value <= 0 ||
-            getProductQuantity.value > 100
-        ) {
-            alert("Veuillez choisir une quantité corecte");
-        } else {
-            addBasket(basketValue);
-        }
+      saveBasket(basketValue);
+      alert(
+        `Le ${object.name} de couleur ${
+          document.querySelector("#colors").value
+        } a été ajouté en ${
+          document.querySelector("#quantity").value
+        } exemplaires à votre panier !`
+      );
     });
-})
-.catch(function (err) {
-    console.log(err);
-})
+}
+
+function saveBasket(basketValue) {
+  localStorage.setItem("product", JSON.stringify(basketValue));
+}
+
+function getBasket() {
+  let basketValue = JSON.parse(localStorage.getItem("product"));
+  if (basketValue === null) {
+    return [];
+  } else {
+    return basketValue;
+  }
+}
